d5e4b306b1471a47ef7e393e4b9715f1995af116
Steven Murawski <steven.murawski@gmail.com>
2017-08-21 15:45:06 -0500

    tweak build
    Signed-off-by: Steven Murawski <steven.murawski@gmail.com>

--- build.ps1
+++ build.ps1
@@ -1,9 +1,10 @@
+$WorkingDir = $env:AGENT_WORKFOLDER
 if (-not get-command hugo -ErrorAction SilentlyContinue) {
-    invoke-webrequest -UseBasicParsing -Uri https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Windows-64bit.zip -OutFile hugo.zip
-    Unblock-File hugo.zip
-    mkdir $env:UserProfile/bin -force
-    Expand-Archive -Path hugo.zip -DestinationPath $env:UserProfile/bin
-    $env:Path += ";$env:UserProfile/bin"
+    invoke-webrequest -UseBasicParsing -Uri 'https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Windows-64bit.zip' -OutFile $WorkingDir/hugo.zip
+    Unblock-File $WorkingDir/hugo.zip
+    mkdir $WorkingDir/bin -force
+    Expand-Archive -Path hugo.zip -DestinationPath $WorkingDir/bin
+    $env:Path += ";$WorkingDir/bin"
 }
 
 hugo -d ./public/staging -b "http://minimallyviable-staging.azurewebsites.net/" 
